b368c450ad1e9b1e6b20f9c423f51ed175b7ffd9
bsg-gukim <gukim@bsgglobal.com>
2023-08-30 23:17:18 +0900

    Spring Exception

--- src/main/java/com/example/exception_demo2/advice/GlobalControllerAdvice.java
+++ src/main/java/com/example/exception_demo2/advice/GlobalControllerAdvice.java
@@ -1,2 +1,28 @@
-package com.example.exception_demo2.advice;public class GlobalControllerAdvice {
+package com.example.exception_demo2.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity exception(Exception e) {
+
+        System.out.println(e.getClass().getName());
+        System.out.println("=====================");
+        System.out.println(e.getLocalizedMessage());
+        System.out.println("=====================");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity nullPointerException(NullPointerException e){
+
+        System.out.println(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 }
--- src/main/java/com/example/exception_demo2/controller/ApiController.java
+++ src/main/java/com/example/exception_demo2/controller/ApiController.java
@@ -1,2 +1,39 @@
-package com.example.exception_demo2.controller;public class ApiController {
+package com.example.exception_demo2.controller;
+
+
+import com.example.exception_demo2.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/user")
+    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+
+        User user = new User();
+
+        user.setName(name);
+        user.setAge(age);
+
+        int a = 10 + age;
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User post(@RequestBody User user) {
+        System.out.println(user);
+
+        return user;
+    }
+
+    // Controller에 있는 ExceptionHandler가 우선순위가 더 높음
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity nullPointerException(NullPointerException e){
+
+        System.out.println("api controller");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 }
